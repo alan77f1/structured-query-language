676beaffcf8f26d0580362a50da4cd91735a2606
buivantan01 <buivantan1234566@gmail.com>
2021-10-09 21:04:31 +0700

    Update lessons

--- ThietKeCSDL/TRIGGER.sql
+++ ThietKeCSDL/TRIGGER.sql
@@ -99,75 +99,16 @@ HOADON (MaHD, NgayLap, MaKH, MaNV, Tongtien, Chietkhau)
 KhachHang (MaKH, TenKH, DiaChi, Dienthoai, Loaithe, Ngaycapthe)
 Nhanvien (MaNV, TenNV, Diachi, Dienthoai)
 
-
-
-
-1. Lập báo cáo doanh thu trong năm 2019 của từng mặt hàng.
-
-select HH.MaH, HH.TenH, sum(CTDH.Soluong) AS 'SL', sum(CTDH.Thanhtien) AS 'TT'
-from ChitietDonHang CTDH inner join HANGHOA HH ON CTDH.MaH = HH.MaH 
-	inner join HOADON HD ON CTDH.MaHD = HD.MaHD
-where Year(HD.NgayLap) = 2021 
-group by HH.MaH, HH.TenH
-
-
-
-
-
-2. Đưa ra danh sách những nhân viên có doanh thu bán hàng tháng 12/2019
-từ 20 triệu trở lên
-
-select NV.MaNV, NV.TenNV, sum(Tongtien) AS 'Doanh thu ban hang'
-from HOADON HD inner join Nhanvien NV ON HD.MaNV = NV.MaNV
-where MONTH(HD.NgayLap) = 11 AND YEAR(HD.NgayLap) = 2021
-group by NV.MaNV, NV.TenNV
-having sum(Tongtien) >= 0
-
-
-
-
-
-3. Tính tổng doanh số mua hàng tính từ ngày cấp thẻ đến ngày hiện tại của
-mỗi khách hàng được cấp thẻ « SoftCard ». Thời gian tính từ ngày cấp
-thẻ đến ngày hiện tại phải &lt;= 1 năm. Chi tiết hiển thị các khách hàng có
-tổng doanh số mua hàng &gt;= 5.000.000
-
-select KH.MaKH, KH.TenKH, sum(HD.Tongtien) as 'Doanh so mua'
-from HOADON HD inner join KhachHang KH ON HD.MaKH = KH.MaKH
-where HD.NgayLap BETWEEN KH.Ngaycapthe AND GETDATE() 
-AND DATEDIFF(day, KH.Ngaycapthe, GETDATE()) < 365 
-AND KH.Loaithe = 'SoftCard'
-group by KH.MaKH, KH.TenKH
-having sum(HD.Tongtien) >= 0
-
-
-
-
-
-
-
-4. Thống kê doanh thu bán hàng theo từng năm của 5 năm trở lại đây.
-
-select YEAR(NgayLap), SUM(Tongtien) AS 'doanh so'
-from HOADON
-group by YEAR(NgayLap)
-having  DATEDIFF(year, YEAR(NgayLap), YEAR(GETDATE())) < 5 AND SUM(Tongtien) >= 0
-
-
-
-
-
-
 5. Viết 1 trigger tự động cập nhật trường Tongtien, Chietkhau trong quan hệ
 HOADON khi thêm mới hay cập nhật một bản ghi trong ChitietDonhang.
 Biết rằng :
 Tongtien=Tổng các Thanhtien của từng mặt hàng trong ChitietDonhang
 Chietkhau = Tongtien * n
-Trong đó : n = 2% nếu khách hàng có thẻ « SoftCard » ;
-n=4% nếu khách hàng có thẻ « SilverCard » ;
-n=5% nếu khách hàng có thẻ « GoldednCard » ;
-n=4% nếu khách hàng có thẻ « DiamondCard » ;
-
+Trong đó : 
+n = 2% nếu khách hàng có thẻ « SoftCard » ;
+n = 4% nếu khách hàng có thẻ « SilverCard » ;
+n = 5% nếu khách hàng có thẻ « GoldednCard » ;
+n = 4% nếu khách hàng có thẻ « DiamondCard » ;
 create trigger tr_HOADON_Tongtien_chietkhau
 on HOADON
 for insert, update
@@ -183,8 +124,7 @@ begin
 	from ChitietDonHang
 	where MaHD = @MaHD
 
-	update HOADON
-	set Tongtien = @Tongtien
+	update HOADON set Tongtien = @Tongtien
 	where MaHD = @MaHD
 
 	select @loaithe = Kh.Loaithe
@@ -204,14 +144,104 @@ begin
 	end
 	else
 	begin
-		update HOADON
-		set Chieckhau = Tongtien * 5 / 100
+		update HOADON set Chieckhau = Tongtien * 5 / 100
 		where MaHD = @MaHD
 	end
 end
 
+1. Lập báo cáo doanh thu trong năm 2019 của từng mặt hàng.
+
+select HH.MaH, HH.TenH, sum(CTDH.Soluong) AS 'SL', sum(CTDH.Thanhtien) AS 'TT'
+from ChitietDonHang CTDH inner join HANGHOA HH ON CTDH.MaH = HH.MaH 
+	inner join HOADON HD ON CTDH.MaHD = HD.MaHD
+where Year(HD.NgayLap) = 2021 
+group by HH.MaH, HH.TenH
 
 
+2. Đưa ra danh sách những nhân viên có doanh thu bán hàng tháng 12/2019
+từ 20 triệu trở lên
+
+select NV.MaNV, NV.TenNV, sum(Tongtien) AS 'Doanh thu ban hang'
+from HOADON HD inner join Nhanvien NV ON HD.MaNV = NV.MaNV
+where MONTH(HD.NgayLap) = 11 AND YEAR(HD.NgayLap) = 2021
+group by NV.MaNV, NV.TenNV
+having sum(Tongtien) >= 0
+
+
+
+3. Tính tổng doanh số mua hàng tính từ ngày cấp thẻ đến ngày hiện tại của
+mỗi khách hàng được cấp thẻ « SoftCard ». Thời gian tính từ ngày cấp
+thẻ đến ngày hiện tại phải &lt;= 1 năm. Chi tiết hiển thị các khách hàng có
+tổng doanh số mua hàng &gt;= 5.000.000
+
+select KH.MaKH, KH.TenKH, sum(HD.Tongtien) as 'Doanh so mua'
+from HOADON HD inner join KhachHang KH ON HD.MaKH = KH.MaKH
+where HD.NgayLap BETWEEN KH.Ngaycapthe AND GETDATE() 
+AND DATEDIFF(day, KH.Ngaycapthe, GETDATE()) < 365 
+AND KH.Loaithe = 'SoftCard'
+group by KH.MaKH, KH.TenKH
+having sum(HD.Tongtien) >= 0
+
+
+
+
+4. Thống kê doanh thu bán hàng theo từng năm của 5 năm trở lại đây.
+
+select YEAR(NgayLap), SUM(Tongtien) AS 'doanh so'
+from HOADON
+group by YEAR(NgayLap)
+having  DATEDIFF(year, YEAR(NgayLap), YEAR(GETDATE())) < 5 AND SUM(Tongtien) >= 0
+
+
+
+5. Viết 1 trigger tự động cập nhật trường Tongtien, Chietkhau trong quan hệ
+HOADON khi thêm mới hay cập nhật một bản ghi trong ChitietDonhang.
+Biết rằng :
+Tongtien=Tổng các Thanhtien của từng mặt hàng trong ChitietDonhang
+Chietkhau = Tongtien * n
+Trong đó : 
+n = 2% nếu khách hàng có thẻ « SoftCard » ;
+n = 4% nếu khách hàng có thẻ « SilverCard » ;
+n = 5% nếu khách hàng có thẻ « GoldednCard » ;
+n = 4% nếu khách hàng có thẻ « DiamondCard » ;
+
+create trigger tr_HOADON_Tongtien_chietkhau
+on HOADON
+for insert, update
+as
+begin
+	declare @MaHD char(10)
+	declare @Tongtien float
+	declare @loaithe char(20)
+
+	select @MaHD = MaHD from inserted
+	
+	select @Tongtien = sum(Thanhtien)
+	from ChitietDonHang
+	where MaHD = @MaHD
+
+	update HOADON set Tongtien = @Tongtien
+	where MaHD = @MaHD
+
+	select @loaithe = Kh.Loaithe
+	from inserted i inner join KhachHang KH ON i.MaKH = KH.MaKH
+
+	IF @loaithe = 'SoftCard'
+	begin
+		update HOADON set Chieckhau = Tongtien * 2 / 100
+		where MaHD = @MaHD
+	end
+	else IF @loaithe IN ('SilverCard', 'DiamondCard')
+	begin
+		update HOADON set Chieckhau = Tongtien * 4 / 100
+		where MaHD = @MaHD
+	end
+	else
+	begin
+		update HOADON set Chieckhau = Tongtien * 5 / 100
+		where MaHD = @MaHD
+	end
+end
 
 
 
@@ -236,12 +266,10 @@ begin
 	where MaH = @MaH
         For each row
         begin
-	   update ChitietDonHang
-	   set Mucgia = @Mucgia, Thanhtien = Soluong * @Mucgia
+	   update ChitietDonHang set Mucgia = @Mucgia, Thanhtien = Soluong * @Mucgia
 	   where MaCTDH = @MaCTHD
         end
 end
-
 
 
 
